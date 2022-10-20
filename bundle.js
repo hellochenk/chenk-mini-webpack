81c098e82d50e11405914b75e6b808244b0bdc7c
chenk <657301050@qq.com>
2022-10-20 15:54:32 +0800

    f: modules & build

--- bundle.js
+++ bundle.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
-
+const babel = require('@babel/core');
 // 关键字： AST 抽象语法书
 // webpack 构建/打包/压缩 等等一系列操作都需要基于AST进行
 
@@ -13,6 +13,10 @@ const getModuleInfo = (file) => {
     // 表示我们要解析的是es6模块
     sourceType: 'module',
   });
+  // ---- 查看ast结构 ----
+  // console.log(ast);
+  // console.log('------------------------')
+  // console.log(ast.program.body)
   const deps = {};
   // 转换ast
   // 使用 traverse 遍历 ast
@@ -25,9 +29,77 @@ const getModuleInfo = (file) => {
       deps[node.source.value] = absPath;
     },
   });
-  console.log(deps);
-  // console.log(ast);
-  // console.log('------------------------')
-  // console.log(ast.program.body)
+  // console.log(deps);
+
+  // 经过 babel 编译后的的代码
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ['@babel/preset-env'],
+  });
+
+  /**
+   * @param file 文件路径
+   * @param deps 依赖信息
+   * @param code 编译后的代码
+   */
+  const moduleInfo = { file, deps, code };
+  console.log(moduleInfo);
+  return moduleInfo;
 };
-getModuleInfo('./src/index.js');
+
+const parseModules = (file) => {
+  // 定义依赖图
+  const depsGraph = {};
+  // 首先获取入口的信息
+  const entry = getModuleInfo(file);
+
+  const temp = [entry];
+  for (let i = 0; i < temp.length; i++) {
+    const item = temp[i];
+    const deps = item.deps;
+    if (deps) {
+      // 遍历模块的依赖，递归获取模块信息
+      for (const key in deps) {
+        if (deps.hasOwnProperty(key)) {
+          temp.push(getModuleInfo(deps[key]));
+        }
+      }
+    }
+  }
+  temp.forEach((moduleInfo) => {
+    depsGraph[moduleInfo.file] = {
+      deps: moduleInfo.deps,
+      code: moduleInfo.code,
+    };
+  });
+  console.log(depsGraph);
+  return depsGraph;
+};
+
+// 生成最终可以在浏览器运行的代码
+const bundle = file => {
+  const depsGraph = JSON.stringify(parseModules(file))
+  return `(function(graph){
+      function require(file) {
+          var exports = {};
+          function absRequire(relPath){
+              return require(graph[file].deps[relPath])
+          }
+          (function(require, exports, code){
+              eval(code)
+          })(absRequire, exports, graph[file].code)
+          return exports
+      }
+      require('${file}')
+  })(${depsGraph})`
+}
+
+build = file => {
+  const content = bundle(file)
+  // 写入到dist/bundle.js
+  fs.mkdirSync('./dist')
+  fs.writeFileSync('./dist/bundle.js', content)
+}
+
+build('./src/index.js')
+
+// getModuleInfo('./src/index.js');
